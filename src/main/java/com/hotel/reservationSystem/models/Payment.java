e9765725bf8f0c5db263af4e2ead3d808df5c968
kishkolh <kishkolh@fel.cvut.cz>
2023-11-08 14:17:42 +0100

    layout fix

--- src/main/java/com/hotel/reservationSystem/models/Payment.java
+++ src/main/java/com/hotel/reservationSystem/models/Payment.java
@@ -5,17 +5,25 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.util.Date;
 
 @Entity
+
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column
     private Double amount;
+
     @Column(name = "bill_number")
     private int billNumber;
+
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date date;
+
+    public Payment() {
+
+    }
 
     public Payment(Double amount, int billNumber, Date date) {
         this.amount = amount;
@@ -23,7 +31,32 @@ public class Payment {
         this.date = date;
     }
 
-    public Payment() {
+    public Integer getId() {
+        return id;
+    }
 
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public int getBillNumber() {
+        return billNumber;
+    }
+
+    public void setBillNumber(int billNumber) {
+        this.billNumber = billNumber;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
--- src/main/java/com/hotel/reservationSystem/models/User.java
+++ src/main/java/com/hotel/reservationSystem/models/User.java
@@ -7,18 +7,28 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column
     private int phone;
+
     @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name")
     private String lastName;
+
     @Column
     private String password;
+
     @Column
     private String email;
+
     @Enumerated(EnumType.STRING)
     private ROLE role;
+
+    public User() {
+
+    }
 
     public User(int phone, String firstName, String lastName, String password, String email, ROLE role) {
         this.phone = phone;
@@ -29,7 +39,55 @@ public class User {
         this.role = role;
     }
 
-    public User() {
+    public Integer getId() {
+        return id;
+    }
 
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(ROLE role) {
+        this.role = role;
     }
 }
