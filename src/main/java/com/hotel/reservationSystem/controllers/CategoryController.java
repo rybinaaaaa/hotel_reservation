7d140d79933c7552b81bdbb161308d3068522b85
kishkolh <kishkolh@fel.cvut.cz>
2023-11-10 18:21:25 +0100

    add controllers package

---
+++ src/main/java/com/hotel/reservationSystem/controllers/CategoryController.java
@@ -0,0 +1,9 @@
+package com.hotel.reservationSystem.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+}
