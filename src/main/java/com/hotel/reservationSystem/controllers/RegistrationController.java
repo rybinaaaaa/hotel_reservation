29b0b57dc2c64b3bff831d232aa405001837cba9
kishkolh <kishkolh@fel.cvut.cz>
2023-12-29 17:54:22 +0100

    registration fix

--- src/main/java/com/hotel/reservationSystem/controllers/RegistrationController.java
+++ src/main/java/com/hotel/reservationSystem/controllers/RegistrationController.java
@@ -7,13 +7,14 @@ import com.hotel.reservationSystem.services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class RegistrationController {
 
