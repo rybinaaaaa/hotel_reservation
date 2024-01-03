0a8a42568190a3478bd242ff4b4ab1ca32b19e41
rybinali <rybinaaaaa.a@gmail.com>
2024-01-03 16:19:11 +0100

    push registration

--- src/main/java/com/hotel/reservationSystem/dto/UserDTO.java
+++ src/main/java/com/hotel/reservationSystem/dto/UserDTO.java
@@ -27,21 +27,32 @@ public class UserDTO {
     @NotEmpty
     private String password;
 
-
-    public String getEmail() {
-        return email;
-    }
-
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getPhone() {
         return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
@@ -50,5 +61,9 @@ public class UserDTO {
 
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
