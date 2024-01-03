3feb7f5923469eb410ca12360ad80bff723423cc
rybinali <rybinaaaaa.a@gmail.com>
2024-01-03 16:46:17 +0100

    add encoder

--- src/main/java/com/hotel/reservationSystem/security/SecurityConfig.java
+++ src/main/java/com/hotel/reservationSystem/security/SecurityConfig.java
@@ -6,27 +6,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auths -> auths
-                        .requestMatchers( "auth/registration", "/error").permitAll()
-                        .anyRequest().hasAnyRole("USER", "ADMIN"))
-
-        ;
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(authz -> authz
+                        .requestMatchers("/auth/registration").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
         return http.build();
     }
 
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
